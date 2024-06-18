c76ee7c6a607fa217973861395f1469bcf46de2d
Kevin Patrick Tan <tankpst@gmail.com>
2024-06-18 18:18:30 +0800

    Simplified the code for retrieving file type in kotlin.

--- src/android/FileManager.kt
+++ src/android/FileManager.kt
@@ -3,7 +3,6 @@ package com.kepat.cordova.file.manager
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import android.webkit.MimeTypeMap
 
 import org.apache.cordova.CordovaPlugin
 import org.apache.cordova.CallbackContext
@@ -63,27 +62,20 @@ class FileManager : CordovaPlugin() {
 
 		// Prepare the required information
 		val intent = Intent(Intent.ACTION_VIEW)
-
-		// Prepare the intent based on mime type
-		val mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(MimeTypeMap.getFileExtensionFromUrl(filePath))
-		if (mimeType != null) {
-			intent.setDataAndType(fileUri, mimeType)
-		} else {
-			intent.setData(fileUri)
-		}
-
+		// Pass the data
+		intent.setDataAndType(fileUri, context.contentResolver.getType(fileUri))
 		// Start activity in a new task
 		intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
 
 		// Check if an app can handle the intent
 		if (intent.resolveActivity(context.packageManager) != null) {
-			// Sart the activity
+			// Start the activity
 			context.startActivity(intent)
-			// No app found to open the file
-			callbackContext.success("The file can be opened by another app");
+			// Found an app to open a the file
+			callbackContext.success("The file can be opened by another app.");
 		} else {
 			// No app found to open the file
-			callbackContext.error("Expecting a valid app to open this file type");
+			callbackContext.error("Expecting a valid app to open this file type.");
 		}
 	}
 }
