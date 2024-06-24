8d35620cb5a0e1b07df95ea7aab04d81fd98f7c9
Kevin Patrick Tan <tankpst@gmail.com>
2024-06-24 18:51:12 +0800

    Enhanced the logic by catering for both files and contents.

--- src/android/Helper.kt
+++ src/android/Helper.kt
@@ -7,7 +7,8 @@ import android.os.Build
 import androidx.core.content.FileProvider
 
 import java.io.File
-import java.net.URISyntaxException
+import java.net.MalformedURLException
+import java.net.URL
 
 /**
  * Helper - Contains the helper to manage the uri 
@@ -24,28 +25,26 @@ class Helper(val context: Context) {
 	public fun getUri(
 		filePath: String
 	): Uri? {
-        // Try parsing as uri directly first
         try {
-            return Uri.parse(filePath)
-        } catch (error: URISyntaxException) {
-            // Try checking as a file path
-            val file = File(filePath)
+            // Try parsing the uri to file first
+            val file = File(URL(filePath).toURI())
 
             // Check if file exists
             if (!file.exists()) {
                 return null
             }
 
-            // Retrieve the uri
-            // Handle potential issues with external storage (Android 10+)
+            // Retrieve the accessible content
             val fileUri: Uri
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 fileUri = FileProvider.getUriForFile(context, context.packageName + ".provider", file)
             } else {
                 fileUri = Uri.fromFile(file)
             }
-
             return fileUri
+        } catch (error: MalformedURLException) {
+            // Try parsing to a Uri
+            return Uri.parse(filePath)
         }
 
         return null
